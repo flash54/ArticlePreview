7a4d3fe60d811db462f47ecfc0393ec57643389d
flash54 <41164323+flash54@users.noreply.github.com>
2021-07-22 00:30:34 +0530

    Desktop Site

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ console.log("hello world");
 const shareBtnClick = ()=>{
 
     const shareBtn = document.querySelector(".shareBtn");
-    const active = document.querySelector(".active");
     const shareBtnActive = document.querySelector(".shareBtnActive");
+    const active = document.querySelector(".active");
 
     shareBtn.addEventListener("click", ()=>{
         
